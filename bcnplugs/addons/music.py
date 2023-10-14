612ab7afef8753a76dae296f9c1419aa59fc0280
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-10-14 17:34:59 +0530

    Update music.py

--- bcnplugs/addons/music.py
+++ bcnplugs/addons/music.py
@@ -1,8 +1,8 @@
 from bcnplugs.addons.help import add_command
 
-module_name = "Help"
+module_name = "Music"
 commands = [
-    ("/help", "help"),
-    ("/ghelp", "ghelp"),
+    ("/play", "play"),
+    ("/vplay", "vplay"),
 ]
 add_command(module_name, commands)
