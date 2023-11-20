4182326d2a7f14059156bb3ac1828b69fa2e2503
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-11-20 19:06:58 +0530

    Update __init__.py

--- bcnplugs/__init__.py
+++ bcnplugs/__init__.py
@@ -27,7 +27,7 @@ app = Client(
     plugins=dict(root="bcnplugs/ass"),
     in_memory=True,
 )
-clients.append(client)
+clients.append(app)
 
 session_list = [SESSION1, SESSION2, 
                 SESSION3, SESSION4, 
