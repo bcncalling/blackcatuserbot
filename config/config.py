1c04094fec13580f2e3a64383a6ed3f840f6a4cd
bcncalling <142149038+bcncalling@users.noreply.github.com>
2024-10-31 18:51:51 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,14 +6,14 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "14688437")) #optional
-API_HASH = getenv("API_HASH", "5310285db722d1dceb128b88772d53a6") #optional
+API_ID = int(getenv("API_ID", "11163590")) #optional
+API_HASH = getenv("API_HASH", "fa76f31e5f7a64d906d4978dd0e5d3b3") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5810389985").split()))
-OWNER_ID = int(getenv("OWNER_ID", "5810389985"))
-BOT_TOKEN = getenv("BOT_TOKEN", "6718954338:AAEPW1Rg8qDDFMmkLlhXrHt9Q7Da-3htrYM")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7398382204").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5750403406"))
+BOT_TOKEN = getenv("BOT_TOKEN", "7350463633:AAE-7oHRLStcf6Wc2ccQNmHZ-rHtts8_zDA")
 
-SESSION1 = getenv("SESSION1", "AQF0iCwAF_OscRVtoaAdzcAijFxSFfxlKLHpL-OP3i0Uamb1-59zV_0-2slJ25f_AoGSsVzkLmR0u221K9zplxvDsuqCUVVsrPNoNwkwmZY9A3varyrMkFZzhcUlii39uygV45FCdpFtERZvwFE3UtLhgEIQlyZhSmU0fAJ8T3mnMVIoTuBhEQ38BU45kFVIKOy4cf9lp8032KTTAeX8_N-duw_uQAc4b2lG5Z54CRlQcxVUWoHomFagFuvaFYICzxFTOoxmc7tT631Q3AzwTuKkPdz5tUEL0uMxD5i5GbfDzwV1bhMU0lNsOyDjjUC17Q2nwMSw-hPVeeCQ-INI5dihAbDIAgAAAAFDBECWAA")
+SESSION1 = getenv("SESSION1", "BQDgILUAbQN9pexKBryrY_JQWFRhW2iIvLdUFEhx-aGRXkPj3WuRJ3UchPySJorQq4sibiuYk6fv_U_AGIe1YDb7j3_cZ6hBY0v-VvStWdjCCv1Jk2JKqWIb5eDRSvkOthhj5RYnnLYdJfMORAIt8eHWjGYwLHDvGU4tQ50UdCwQ8INvqIGGhFoCJv48E68vg3X5kdMWTkQYaxKLqu8_EnA69vs8wn5RiLUjIScTub-zitTB2HEwjPwLBrAgkbNNrVnamYEWA2MfZsj0hyM-pe8VxOf90BAynis8CySqX-ADoTwzuPg6FNLaGTmfO9RLISjicilvU9xsoaB02azuL2uw-S0KIgAAAAFWwDFOAA")
 SESSION2 = getenv("SESSION2", "")
 SESSION3 = getenv("SESSION3", "")
 SESSION4 = getenv("SESSION4", "")
